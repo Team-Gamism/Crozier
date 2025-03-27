eb751498378e9bc77a507852ea4000d321d592a6
magicJSC <jeonghaejun004@gmail.com>
2025-03-27 09:00:28 +0900

    Entity

---
+++ Assets/3.Script/Entity/EntitySO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntitySO", menuName = "Scriptable Objects/EntitySO")]
+public class EntitySO : ScriptableObject
+{
+    public float speed;
+    public float maxHp;
+}
